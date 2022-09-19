6f0a54d481ec486176b2105d12eb19129bc699a0
Luke aka SwissalpS <Luke@SwissalpS.ws>
2022-09-19 21:53:01 +0200

    Create vteDump.sh

---
+++ vteDump.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+pkg-config --list-all|grep vte
+echo "----------------------------------"
+pkg-config --modversion vte-2.91
+echo "----------------------------------"
+pkg-config --modversion vte291
+echo "----------------------------------"
+pkg-config --modversion libvte
+echo "----------------------------------"
+pkg-config --modversion vte
+echo "----------------------------------"
