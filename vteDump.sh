b6ce7927603726e09b766671bcd7e20a5404079d
Luke aka SwissalpS <Luke@SwissalpS.ws>
2022-09-27 10:11:08 +0200

    Update vteDump.sh

--- vteDump.sh
+++ vteDump.sh
@@ -10,7 +10,7 @@ pkg-config --modversion libvte
 echo "> pkg-config --modversion vte"
 pkg-config --modversion vte
 echo "> ls -al /usr/include/vte-291"
-ls -al /usr/include/vte-291
+ls -al /usr/include/vte-2.91
 echo "for https://github.com/nonstop/termit/issues/125#issuecomment-1254119382"
 echo "> pkg-config --cflags vte-2.91"
 pkg-config --cflags vte-2.91
@@ -18,4 +18,6 @@ echo "> pkg-config --libs vte-2.91"
 pkg-config --libs vte-2.91
 echo "> find /usr/include -name vte -type d"
 find /usr/include -name vte -type d
+echo "> ls -l /usr/include/vte-2.91/vte"
+ls -l /usr/include/vte-2.91/vte
 exit 0
